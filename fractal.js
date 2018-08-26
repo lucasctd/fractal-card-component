fed05c871825a1327a1eefd1eb3362c833d76aef
Lucas Reis <lucas@programmer.com.br>
2018-08-26 17:18:50 -0300

    Build app to serve on github
    Added fractal built files to be served at github pages.

--- fractal.js
+++ fractal.js
@@ -30,4 +30,6 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
  */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
 
+fractal.web.set('builder.dest', 'build'); // destination for the static export
+
 module.exports  = fractal;
